a7e7708143a2d26e37c7a90300c8509e2ffb09cb
gutosramos <gutolpj@gmail.com>
2024-09-04 23:13:04 +0000

    teste 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!!!!!!"
+    return "Hello, World!!!! teste de integração"
 
 @app.route('/payload', methods=['POST'])
 def webhook():
