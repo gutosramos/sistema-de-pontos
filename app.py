1b659a2e7729d9cf102b9583380cc3316c85b5ea
gutosramos <gutolpj@gmail.com>
2024-09-04 23:12:49 +0000

    teste de integração

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!!!!!!"
+
+@app.route('/payload', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        subprocess.call(['/var/www/flaskapp/deploy.sh'])
+        return 'Deploy initiated', 200
+    else:
+        return 'Invalid method', 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
 cd /var/www/flaskapp
-
-# Ativar o ambiente virtual
-source venv/bin/activate
-
-# Atualizar o repositório
 git pull origin main
-
-# Instalar dependências
+source /var/www/flaskapp/venv/bin/activate
 pip install -r requirements.txt
-
-# Rodar testes (substitua com seu comando de teste se houver)
-# python -m unittest discover
-
-# Reiniciar o Apache para aplicar as alterações
 sudo systemctl restart apache2
