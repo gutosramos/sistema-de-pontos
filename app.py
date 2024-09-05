c26bd22c73823a2f5b67c8cb1d55f0f9e93ad45a
Gustavo Ramos <38006355+gutosramos@users.noreply.github.com>
2024-09-05 00:04:23 +0000

    integração 001

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!!!! teste de integração 3"
+    return "Hello, World!!!! integração"
 
+@app.route('/payload', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        subprocess.call(['/var/www/flaskapp/deploy.sh'])
+        return 'Deploy initiated', 200
+    else:
+        return 'Invalid method', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
